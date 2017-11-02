21703b2c982591e8d0a8fe92d76400a8872fc7ad
Hannah Danziger <hdanziger8@gmail.com>
2017-11-02 20:06:24 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,10 +22,10 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block
+def can_be_created_in_a_block(title, date)
   movie = Movie.create do |m|
     m.title = title
-    m.release_date = year
+    m.release_date = date
   end
 end
 
