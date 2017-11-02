cd7705446a6437f93810670d7a73f483d1b12ecc
Hannah Danziger <hdanziger8@gmail.com>
2017-11-02 20:31:34 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -55,15 +55,16 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movie.where("release_date > 2000").order("release_date: :desc")
+  Movie.where("release_date > 2000").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  movie = Movie.find_by(title: "Awesome Flick")
+  movie.name (title: "Even Awesomer Flick")
+  movie.save
+  
 end
 
 def can_update_using_update_method
