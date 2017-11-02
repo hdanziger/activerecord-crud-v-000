f104fbeec8a998e586aa5df8d6c2a853bb94e4cf
Hannah Danziger <hdanziger8@gmail.com>
2017-11-02 20:05:25 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,8 @@ end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    m.title = title
-    m.release_date = date
+    m.title = Movie.title
+    m.release_date = Movie.release_date
   end
 end
 
